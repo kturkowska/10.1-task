5cd8b671b210f57918cbb0b0fd318cf8f15a73a4
Katarzyna Turkowska <ka.turkowska@gmail.com>
2018-07-13 23:24:45 +0200

    add button reset

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  pageDots: false;
+});
+
+
+var buttonGroup = document.querySelector('.button-group');
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var selector = event.target.getAttribute('data-selector');
+  flkty.selectCell( selector );
 });
 
