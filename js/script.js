f6d8dffe59a93e4c7cef3187d9370d11b5732140
Katarzyna Turkowska <ka.turkowska@gmail.com>
2018-07-13 23:39:27 +0200

    install hash js file

--- js/script.js
+++ js/script.js
@@ -4,18 +4,27 @@ var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
-  pageDots: false;
+
 });
 
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
 
 var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
 
 buttonGroup.addEventListener( 'click', function( event ) {
   // filter for button clicks
   if ( !matchesSelector( event.target, '.button' ) ) {
     return;
   }
-  var selector = event.target.getAttribute('data-selector');
-  flkty.selectCell( selector );
+  var index = buttons.indexOf( event.target );
+  flkty.selectCell( index );
 });
+
 
