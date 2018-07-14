bf7b675c797b39f2e7ad40312c5c8350b89730c2
Katarzyna Turkowska <ka.turkowska@gmail.com>
2018-07-14 23:22:02 +0200

    add map and api key

--- js/script.js
+++ js/script.js
@@ -52,3 +52,14 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
