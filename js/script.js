27736aa7676df58e2af2f083497bc8a68748dfda
Katarzyna Turkowska <ka.turkowska@gmail.com>
2018-07-15 12:07:20 +0200

    push coords to html and write map function

--- js/script.js
+++ js/script.js
@@ -51,51 +51,32 @@ var progressBar = document.querySelector('.progress-bar')
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
+ 
 });
+
+//flkty.on( 'change',
+//  function( index ){  
+//  map.panTo(slidesData[index].coords); 
+// map.setZoom(10);
+//});
 
 
 (function(){ 
   
     window.initMap = function() {
-    
-    var venice = {lat: 33.986, lng: -118.475};
-    var vegas = {lat: 36.170, lng: -115.139};
-    var canyon = {lat: 36.099, lng: -112.103};
-    var monument = {lat: 37.020, lng: -110.098};
-    var sanfran = {lat: 37.823, lng: -122.479};
-    
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 6,
-      center: venice
-    });
-    
-    var markerOne = new google.maps.Marker({
-      position: venice,
-      map: map
-    });
-    
-    var markerTwo = new google.maps.Marker({
-      position: vegas,
-      map: map
-    });
 
-
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: slidesData[0].coords
+      });
     
-    var markerThree = new google.maps.Marker({
-      position: canyon,
-      map: map
-    });
-
-     var markerThree = new google.maps.Marker({
-      position: monument,
-      map: map
-    });
-
-      var markerThree = new google.maps.Marker({
-      position: sanfran,
-      map: map
-    });
-     
+      for (var i = 0; i < slidesData.length; i++) {
+        
+        var marker = new google.maps.Marker({
+        position: slidesData[i].coords,
+        map: map
+        });
+      }
     } 
    
 })();  
